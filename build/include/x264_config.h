c93fc7d234c9d7f2eef7b18d47b82ddbbc7fb283
Florian P. Nierhaus <fpn@bebo.com>
2018-01-30 18:04:15 -0800

    binaries with log params

--- build/include/x264_config.h
+++ build/include/x264_config.h
@@ -3,6 +3,6 @@
 #define X264_INTERLACED    1
 #define X264_CHROMA_FORMAT 0
 #define X264_REV 2854
-#define X264_REV_DIFF 1
-#define X264_VERSION " r2854+1 386b7ac"
-#define X264_POINTVER "0.152.2854+1 386b7ac"
+#define X264_REV_DIFF 3
+#define X264_VERSION " r2854+3 ea0c687"
+#define X264_POINTVER "0.152.2854+3 ea0c687"
